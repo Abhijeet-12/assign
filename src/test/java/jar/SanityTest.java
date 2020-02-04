f142eb47fb129736b4f98693cfafc7774dd5259a
Abhijeet-12 <garaiabhijeet@gmail.com>
2020-02-04 16:05:21 +0530

    addition

---
+++ src/test/java/jar/SanityTest.java
@@ -0,0 +1,27 @@
+package jar;
+
+import static org.junit.Assert.assertNotNull;
+
+import javax.inject.Inject;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import jar.di.CoreModule;
+import jar.fixture.ContainerRule;
+
+import com.amazonaws.services.ec2.AmazonEC2;
+
+public class SanityTest {
+	@Rule
+	public ContainerRule containerRule = new ContainerRule(new CoreModule());
+	
+	@Inject
+	AmazonEC2 amazonEC2;
+
+	@Test
+	public void sanityCheckTest() {
+		assertNotNull(amazonEC2);
+	}
+
+}
